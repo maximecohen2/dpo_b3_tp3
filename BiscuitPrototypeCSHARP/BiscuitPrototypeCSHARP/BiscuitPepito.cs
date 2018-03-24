2df3b7cbde55cbc6d738bdce67443e6a981b5049
Mathilde T <mathilde.thuringer@gmail.com>
2018-03-24 10:37:58 +0100

    BiscuitCSHARP
    Biscuit Prototype - Design Pattern

---
+++ BiscuitPrototypeCSHARP/BiscuitPrototypeCSHARP/BiscuitPepito.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiscuitPrototypeCSHARP
+{
+    class BiscuitPepito : Biscuit
+    {
+        private String typeChoco;
+
+
+        public override Biscuit Clone()
+        {
+            return (Biscuit)this.MemberwiseClone(); // copie membre à membre par défaut
+        }
+
+
+        public String TypeChoco { get => typeChoco; set => typeChoco = value; }
+    }
+}
